e2dabd8dfef80612d1423184f5f875864802661d
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-09-07 11:45:52 -0400

    Add files via upload

---
+++ lab5.5_q4.cpp
@@ -0,0 +1,22 @@
+//printing rhombus star pattern
+#include <iostream>
+using namespace std;
+int main()
+{
+int a ,i ,j;
+// printing the stars and spaces
+for (int i=0; i<a; i++){
+	for (int j=0; j<a-i;j++){
+		cout << " ";
+		}
+	for(int j=0; j<a;j++){
+		cout<<"*";
+		}
+	for(int j=0; j<i;j++){
+		cout<<" ";
+		}
+	
+cout <<endl;		
+}
+return 0;
+}
